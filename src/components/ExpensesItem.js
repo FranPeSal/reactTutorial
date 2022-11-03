eaea48f64d25f91855679015ae050fb5ead013a9
Fran <franciscopesal@gmail.com>
2022-11-03 16:19:50 +0100

    prueba

---
+++ src/components/ExpensesItem.js
@@ -0,0 +1,21 @@
+import './ExpensesItem.css'
+import ExpenseDate from "./ExpenseDate";
+import Card from './Card';
+
+
+function ExpensesItem(props) {
+
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate
+                date={props.date} />
+            <div className='expense-item__description'>
+                <h2 >{props.title}</h2>
+            </div>
+            <div className='expense-item__price'>{props.amount + '€'} </div>
+        </Card>
+    );
+}
+
+export default ExpensesItem;
